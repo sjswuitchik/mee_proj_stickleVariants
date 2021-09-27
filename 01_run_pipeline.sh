1116ea25f99cee3c540464850412b68b90459276
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-09-27 11:18:41 -0700

    Update 01_run_pipeline.sh

--- 01_run_pipeline.sh
+++ 01_run_pipeline.sh
@@ -1,5 +1,9 @@
 ## in /home/sjsmith/projects/def-jonmee/sjsmith/mee_proj_stickleVariants on Cedar (CC HPC) 
 
+mkdir -p data/muir
+mkdir -p data/shunda
+
 git clone https://github.com/harvardinformatics/shortRead_mapping_variantCalling.git
 cd shortRead_mapping_variantCalling/
+
 
