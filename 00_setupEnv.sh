33ad9cc4eba7b131726e3bca3233d9a4bddb4b73
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-10-21 10:34:57 -0700

    Update 00_setupEnv.sh

--- 00_setupEnv.sh
+++ 00_setupEnv.sh
@@ -1,4 +1,4 @@
 conda install -n base -c conda-forge mamba
 mamba create -c conda-forge -c bioconda -n snakemake snakemake
 conda activate snakemake 
-conda install -n snakemake -c conda-forge pandas yappi
+conda install -n snakemake -c conda-forge pandas yappi pyyaml
