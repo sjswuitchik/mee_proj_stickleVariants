d6b652d3a0e55aa173b6899fde39b7ff171cd769
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-04-14 10:07:42 -0700

    Update 01b_run_shunda.sh

--- 01b_run_shunda.sh
+++ 01b_run_shunda.sh
@@ -2,20 +2,17 @@
 ## FASTQs live in /scratch/sjsmith/shunda
 
 # clone in pipeline repo
-git clone https://github.com/harvardinformatics/shortRead_mapping_variantCalling.git
-git checkout dev # to work with write_samples.py for metadata creation that's in dev 
+git clone https://github.com/harvardinformatics/snpArcher.git
 
 # make sample lists and move to working dir
 cd /scratch/sjsmith/shunda/
-.././brename -e -p "\." -r "_" -R
-.././brename -e -p "_fastq" -r ".fastq" -R 
 ls *_R1.fastq.gz > sR1s.txt
 sed 's/\_R1\.fastq\.gz//g' sR1s.txt > shunda_samples.txt
-mv shunda_samples.txt ~/projects/def-jonmee/sjsmith/mee_proj_stickleVariants/shunda/shortRead_mapping_variantCalling/
+cp shunda_samples.txt ~/projects/def-jonmee/sjsmith/mee_proj_stickleVariants/shunda/shortRead_mapping_variantCalling/
 
 # download ref genome
 cd  ~/projects/def-jonmee/sjsmith/mee_proj_stickleVariants/shunda/shortRead_mapping_variantCalling/
-mkdir data/genome/
+mkdir -p data/genome/
 cd data/genome
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/902/500/615/GCF_902500615.1_NSP_V7/GCF_902500615.1_NSP_V7_genomic.fna.gz
 cd ../../workflow
@@ -24,5 +21,5 @@ cd ../../workflow
 python3 write_samples.py -s ../shunda_samples.txt -f /scratch/sjsmith/shunda/ -r ../data/genome/GCF_902500615.1_NSP_V7_genomic.fna.gz -o Pungitius_pungitius
 cd ..
 
-# run pipeline - nb: to run on Cedar, removed #SBATCH -p shared from run_pipeline.sh bc that is Cannon specific
+# run pipeline - nb: to run on Cedar, removed #SBATCH -p shared from run_pipeline.sh bc that is Cannon specific, added account:  def-jonmee under __default__ in profiles/slurm/cluster_config.yml
 sbatch --account=def-jonmee run_pipeline.sh
