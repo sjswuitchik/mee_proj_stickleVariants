a9857741a04c347da846acf1baeb875f8de81753
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-21 11:29:36 -0700

    Update run_var2tab_muir.sh

--- run_var2tab_muir.sh
+++ run_var2tab_muir.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #SBATCH -J gatkVar
-#SBATCH -o process_out/out
-#SBATCH -e process_out/err
+#SBATCH -o out
+#SBATCH -e err
 #SBATCH -n 1
 #SBATCH -t 9000
 #SBATCH --mem=9000
 
+# submit from muir/snpArcher/process_out
+
 source activate gatk
 
-gatk VariantsToTable -Xmx8g -V process_out/muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O process_out/muir.var.txt
+gatk VariantsToTable -Xmx8g -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O muir.var.txt
