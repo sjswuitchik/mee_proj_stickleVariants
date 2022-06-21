f65f664476a465fba1307b574b7e2a16a1119d71
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-20 19:19:00 -0700

    Update 02_processOutput.sh

--- 02_processOutput.sh
+++ 02_processOutput.sh
@@ -1,8 +1,8 @@
-## in /scratch/sjsmith/shunda/snpArcher
 # set up a GATK4 conda env to convert VCF outside of the snpArcher pipeline
 conda create -n gatk -c bioconda gatk4 vcftools
 conda activate gatk 
 
+## in /scratch/sjsmith/shunda/snpArcher
 mkdir process_out
 cp results/Pungitius_pungitius/GCF_902500615.1_NSP_V7_genomic/Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz* process_out/
 
@@ -13,5 +13,17 @@ mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi shunda.fi
 vcftools --gzvcf shunda.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out shunda.filt
 
 # convert filtered VCF to table for use in multiallelic site removal script
-gatk VariantsToTable -V shunda.filt.vcf -F CHROM -F POS -F AF -F REF -F ALT -O shunda.var.txt
+gatk VariantsToTable -V shunda.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O shunda.var.txt
 
+## in /scratch/sjsmith/muir/snpArcher
+mkdir process_out
+cp results/Pungitius_pungitius/GCF_902500615.1_NSP_V7_genomic/Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz* process_out/
+
+# process VCF 
+cd process_out
+mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz muir.final.vcf.gz
+mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi muir.final.vcf.gz.tbi
+vcftools --gzvcf muir.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out muir.filt
+
+# convert filtered VCF to table for use in multiallelic site removal script
+gatk VariantsToTable -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O muir.var.txt
