f3fcf42e05dc8c6c5ebfeab30ec2ab421fdd1ebd
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-21 11:12:14 -0700

    Update 02_processOutput.sh

--- 02_processOutput.sh
+++ 02_processOutput.sh
@@ -10,7 +10,10 @@ cp results/Pungitius_pungitius/GCF_902500615.1_NSP_V7_genomic/Pungitius_pungitiu
 cd process_out
 mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz shunda.final.vcf.gz
 mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi shunda.final.vcf.gz.tbi
+zgrep -v '^#' shunda.final.vcf.gz | wc -l 
+# 36903496
 vcftools --gzvcf shunda.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out shunda.filt
+#
 
 # convert filtered VCF to table for use in multiallelic site removal script
 gatk VariantsToTable -V shunda.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O shunda.var.txt
@@ -23,7 +26,10 @@ cp results/Pungitius_pungitius/GCF_902500615.1_NSP_V7_genomic/Pungitius_pungitiu
 cd process_out
 mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz muir.final.vcf.gz
 mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi muir.final.vcf.gz.tbi
+zgrep -v '^#' muir.final.vcf.gz | wc -l
+# 36242173
 vcftools --gzvcf muir.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out muir.filt
+#
 
 # convert filtered VCF to table for use in multiallelic site removal script
 gatk VariantsToTable -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O muir.var.txt
