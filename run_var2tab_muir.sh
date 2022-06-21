120daa81b3ccf301d96ab8e07e08b6dfc187c1e8
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-21 11:46:10 -0700

    Update run_var2tab_muir.sh

--- run_var2tab_muir.sh
+++ run_var2tab_muir.sh
@@ -10,4 +10,4 @@
 
 source activate gatk
 
-gatk VariantsToTable -Xmx48g -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -F AF -F DP -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -F TYPE -F FILTER -GF AD -GF DP -GF GQ -GF GT -GF SB -O muir.var.txt --split-multi-allelic
+gatk VariantsToTable -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -F AF -F DP -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -F TYPE -F FILTER -GF AD -GF DP -GF GQ -GF GT -GF SB -O muir.var.txt --split-multi-allelic
