2ba0dcd41caa4a8b35d5d62fb6d5c4b13be5ddb7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-21 11:23:16 -0700

    Update 02_processOutput.sh

--- 02_processOutput.sh
+++ 02_processOutput.sh
@@ -13,7 +13,7 @@ mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi shunda.fi
 zgrep -v '^#' shunda.final.vcf.gz | wc -l 
 # 36903496
 vcftools --gzvcf shunda.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out shunda.filt
-#
+# 2701687
 
 # convert filtered VCF to table for use in multiallelic site removal script
 gatk VariantsToTable -V shunda.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O shunda.var.txt
@@ -29,7 +29,7 @@ mv Pungitius_pungitius_GCF_902500615.1_NSP_V7_genomic.final.vcf.gz.tbi muir.fina
 zgrep -v '^#' muir.final.vcf.gz | wc -l
 # 36242173
 vcftools --gzvcf muir.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out muir.filt
-#
+# 3102019
 
 # convert filtered VCF to table for use in multiallelic site removal script
-gatk VariantsToTable -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O muir.var.txt
+gatk VariantsToTable -Xmx8g -V muir.filt.recode.vcf -F CHROM -F POS -F AF -F REF -F ALT -O muir.var.txt
