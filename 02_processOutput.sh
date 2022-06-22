d92557d7a89b7fe151ef5e52f69bd2611a6f0a3f
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2022-06-22 10:33:51 -0700

    Update 02_processOutput.sh

--- 02_processOutput.sh
+++ 02_processOutput.sh
@@ -14,7 +14,7 @@ zgrep -v '^#' shunda.final.vcf.gz | wc -l
 vcftools --gzvcf shunda.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out shunda.filt
 # 2701687
 
-# convert filtered VCF to table for use in multiallelic site removal script
+# convert filtered VCF to table for use in multiallelic recode script
 sbatch --account=def-jonmee run_var2tab_shunda.sh
 
 # run filtering script
@@ -33,7 +33,7 @@ zgrep -v '^#' muir.final.vcf.gz | wc -l
 vcftools --gzvcf muir.final.vcf.gz --max-missing 0.95 --maf 0.01 --recode --recode-INFO-all --out muir.filt
 # 3102019
 
-# convert filtered VCF to table for use in multiallelic site removal script
+# convert filtered VCF to table for use in multiallelic recode script
 sbatch --account=def-jonmee run_var2tab_muir.sh
 
 # run filtering script
