6cf3afb9661633e87288f75fe6ceb0d758fb6f2f
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-08-31 13:55:21 -0600

    Update 03_mk_tests.sh

--- 03_mk_tests.sh
+++ 03_mk_tests.sh
@@ -1,3 +1,11 @@
 # process gasAcu outgroup aligned to punPun with snpArcher from BioProject PRJNA639125
 # in /scratch/sjsmith/mk_tests
 
+conda env create -f vcfSnpEff.yml
+
+#### Muir #### 
+cd muir_gasAcu
+
+
+#### Shunda ####
+cd shunda_gasAcu
