b8f82064c51fe6aa9784a4cddf839c514f0be8c0
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-15 13:39:37 -0600

    Update 03_mk_tests.sh

--- 03_mk_tests.sh
+++ 03_mk_tests.sh
@@ -2,9 +2,6 @@
 # in /scratch/sjsmith/mk_tests
 ## based off https://github.com/sjswuitchik/compPopGen_ms/tree/master/MKpipeline
 
-conda env create -f vcfSnpEff.yml
-conda env create -f r.yml
-
 #### Muir #### 
 cd muir_gasAcu
 
