a5b73388b533170e9c9bdbaa1924975347e7403f
sjswuitchik <ssmith6@mtroyal.ca>
2022-09-16 11:27:58 -0700

    Create missingness.R

---
+++ scripts/missingness.R
@@ -0,0 +1,20 @@
+#!/usr/bin/Rscript
+
+library(tidyverse)
+
+args <- commandArgs(trailingOnly = T)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).\n", call.=FALSE)
+} 
+
+ingroup <- read_delim(args[1], delim = '\t', col_names = T) %>%
+  group_by(INDV) %>%
+  summarise_each(funs(sum)) %>%
+  mutate(missing = N_MISS/N_DATA)
+threshold <- 3*median(ingroup$missing)
+remove <- ingroup %>% 
+  filter(missing >= threshold) %>%
+  select(INDV) %>%
+  write_delim(., "ingroup.remove.indv", delim = '\t', col_names = T)
