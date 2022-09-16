e2bc10d41840a0bd70164bdc6bf1e6282e071d19
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-16 12:53:50 -0600

    Update 03_mk_tests.sh

--- 03_mk_tests.sh
+++ 03_mk_tests.sh
@@ -3,6 +3,20 @@
 ## based off https://github.com/sjswuitchik/compPopGen_ms/tree/master/MKpipeline
 
 conda activate snpeff
+cd snpeff 
+mkdir -p data/genome
+cd data/genome
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/902/500/615/GCF_902500615.1_NSP_V7/GCF_902500615.1_NSP_V7_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/902/500/615/GCF_902500615.1_NSP_V7/GCF_902500615.1_NSP_V7_genomic.gff.gz
+mv GCF_902500615.1_NSP_V7/GCF_902500615.1_NSP_V7_genomic.fna.gz sequences.fa.gz
+mv GCF_902500615.1_NSP_V7/GCF_902500615.1_NSP_V7_genomic.gff.gz genes.gff.gz
+cp -v genes.gff.gz ../muir_gasAcu
+cp -v genes.gff.gz ../shunda_gasAcu
+
+cd ../..
+# edit the config to include genome info
+sbatch snpeff_db.sh
+
 #### Muir #### 
 cd muir_gasAcu
 vcftools --gzvcf muir.final.vcf.gz --missing-indv --out muir
