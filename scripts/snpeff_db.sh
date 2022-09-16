5a6c6d66cbecbe4de84891db70152dcf7fd960ab
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-16 12:54:45 -0600

    Update snpeff_db.sh

--- scripts/snpeff_db.sh
+++ scripts/snpeff_db.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#SBATCH -J snpeff_db
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=9000
 
+conda activate snpeff
 
 snpEff -Xmx8g build -c snpEff.config -gff3 -v -noCheckCds -noCheckProtein punPun
