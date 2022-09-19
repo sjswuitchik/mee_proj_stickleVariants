183bb34391d9adb96a7ee224649f9fe98b4b1bd1
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-19 14:26:21 -0600

    Update 00_setupEnvs.sh

--- 00_setupEnvs.sh
+++ 00_setupEnvs.sh
@@ -9,6 +9,9 @@ conda create -n gatk -c bioconda -c conda-forge gatk4 vcftools pandas numpy
 
 # for MK tests - was having issues with conda on Cedar building from the yml files, so doing it this way instead
 conda create -n snpeff -c bioconda -c ets snpeff jdk11-ac
-conda activate snpeff
-conda install -c bioconda -c conda-forge cyvcf2 tqdm bcftools vcftools htslib bedtools 
-conda install -c bioconda -c conda-forge -c r r-base r-tidyverse r-r2jags r-lme4 r-arm
+
+conda create -n r -c bioconda -c conda-forge -c r r-base r-tidyverse r-r2jags r-lme4 r-arm
+
+conda create -n vcf -c bioconda -c conda-forge bcftools vcftools htslib bedtools
+conda activate vcf 
+conda install -c bioconda -c conda-forge -c asmeurer glibc libgcc-ng cyvcf2 tqdm 
