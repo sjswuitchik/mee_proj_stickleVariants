3fce0c0a7b5709435b21de40fbacff2f25a1e187
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-20 10:37:17 -0600

    Create prep_snipre.sh

---
+++ scripts/prep_snipre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J snipre
+#SBATCH -o snip_out
+#SBATCH -e snip_err
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=0
+#SBATCH --exclusive
+
+# submit from /scratch/sjsmith/mk_tests/muir_gasAcu
+
+mamba activate r
+
+Rscript --slave --vanilla ../helper_scripts/prep_snipre.R muir.final.bed gasAcu.final.bed muir.lmiss gasAcu.lmiss
+
+Rscript --slave --vanilla ../helper_scripts/run_snipre.R
