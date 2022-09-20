e683979f2ad5d27832afacae2189dc781971564c
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-20 10:27:51 -0600

    Create run_snpEff_shunda.sh

---
+++ scripts/run_snpEff_shunda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J snpEff
+#SBATCH -o snpEff_out
+#SBATCH -e snpEff_err
+#SBATCH -n 1
+#SBATCH -t 9000
+#SBATCH --mem=0
+#SBATCH --exclusive
+
+# submit from /scratch/sjsmith/mk_tests/snpeff
+
+mamba activate snpeff
+
+snpEff ann -Xmx8g -i vcf -o vcf -c snpEff.config punPun /scratch/sjsmith/mk_tests/shunda_gasAcu/shunda.clean.vcf > shunda.ann.vcf
+
