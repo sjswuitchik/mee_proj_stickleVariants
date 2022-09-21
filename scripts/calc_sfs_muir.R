2ebb1d9054750e10ffa7740ddc47929154f61800
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-21 13:25:00 -0600

    Create calc_sfs_muir.R

---
+++ scripts/calc_sfs_muir.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+#read in data, rename col names
+data <- read_delim("muir.frq.count", delim = '\t', col_names = T) %>%
+  rename(chrom = CHROM, pos = POS, n_alleles = N_ALLELES, n_chr = N_CHR, major = MAJOR, AC = MINOR) 
+
+# calc folded SFS
+fold <- data %>%
+  mutate(fold = ifelse(AC > 15, 30 - AC, AC)) %>%
+  filter(fold > 0)
+# put SFS into its own var
+sfs <- table(fold$fold)
+# check SFS
+pdf("muir.sfs.pdf")
+plot(sfs)
+dev.off()
+# write out so you can manually put into the Stairway blueprint file 
+sfs %>% 
+  as.data.frame() %>%
+  write_delim(., "muir.sfs.tsv", delim = '\t', col_names = T)
