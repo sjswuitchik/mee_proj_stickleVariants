7a17a0ac0d3a72383c83b6e013029ee643dca245
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-21 13:22:39 -0600

    Update 00_setupEnvs.sh

--- 00_setupEnvs.sh
+++ 00_setupEnvs.sh
@@ -13,7 +13,6 @@ mamba create -n snpeff -c bioconda -c ets snpeff jdk11-ac
 mamba create -n r -c conda-forge r-tidyverse r-r2jags r-lme4 r-arm
 
 mamba create -n vcf -c bioconda -c conda-forge bcftools vcftools htslib bedtools cyvcf2 tqdm 
-mamba activate vcf 
-#mamba install -c conda-forge -c asmeurer glibc libgcc-ng 
+
 
 
