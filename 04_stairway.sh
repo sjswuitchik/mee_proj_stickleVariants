ffe9e823592e5ef8bac6a6266890579620a0051e
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-21 13:50:04 -0600

    Update 04_stairway.sh

--- 04_stairway.sh
+++ 04_stairway.sh
@@ -1,18 +1,17 @@
-# in /scratch/sjsmith/stairway/muir
+# in /scratch/sjsmith/
 
 module load vcftools r 
 # vcftools version 0.1.16, r v. 4.2.1
 
 git clone https://github.com/xiaoming-liu/stairway-plot-v2.git
+mv stairway-plot-v2/ stairway/
+cd stairway
 unzip stairway_plot_v2.1.1.zip
 rm stairway_plot_v2.1.1.zip
-mv stairway-plot-v2/ stairway/
-mkdir -p stairway/muir
-mkdir -p stairway/shunda
 
-cp muir_gasAcu/muir.clean.vcf stairway/muir
+cp ../muir_gasAcu/muir.clean.vcf .
 
 vcftools --vcf muir.clean.vcf --max-missing 1 --min-alleles 2 --max-alleles 2 --remove-filtered-all --remove-indels --out muir --counts2
 
 # edit frq.counts header to be CHROM  POS N_ALLELES N_CHR MAJOR MINOR
-Rscript calc_sfs.R
+Rscript calc_sfs_muir.R
