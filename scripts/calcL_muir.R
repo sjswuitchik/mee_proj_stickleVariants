62f8819c91d12b86123ed0594ad20df7e78e0da0
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-09-21 14:40:18 -0600

    Create calcL_muir.R

---
+++ scripts/calcL_muir.R
@@ -0,0 +1,8 @@
+library(readr)
+library(magrittr)
+library(dplyr)
+
+data <- read_delim("muir.callable_sites_cov.bed", delim = '\t', col_names = F) %>%
+  rename(chr = X1, start = X2, end = X3) %>%
+  mutate(int_len = end-start)
+sum(data$int_len)
