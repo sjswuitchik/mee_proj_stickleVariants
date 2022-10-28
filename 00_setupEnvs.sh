e4bec0a5d8ddf2214719f805f53d03192d713a92
Sara Smith Wuitchik <ssmith6@mtroyal.ca>
2022-10-28 10:00:25 -0600

    Update 00_setupEnvs.sh

--- 00_setupEnvs.sh
+++ 00_setupEnvs.sh
@@ -2,7 +2,7 @@
 conda install -n base -c conda-forge mamba
 mamba create -c conda-forge -c bioconda -n snakemake snakemake
 conda activate snakemake 
-conda install -n snakemake -c conda-forge pandas yappi pyyaml
+conda install -n snakemake -c conda-forge pandas yappi pyyaml perl
 
 # for output processing
 conda create -n gatk -c bioconda -c conda-forge gatk4 vcftools pandas numpy
